c0676dc2ee0685b776e9baab977198e329c02e38
Cesar Aleman <cesar.aleman.cal@gmail.com>
2022-04-09 14:37:02 -0700

    another attempt

--- php/config.php
+++ php/config.php
@@ -1,7 +1,7 @@
 <?php 
     // $conn = mysqli_connect("localhost", "root", "", "securechatapp");
-    $conn = mysqli_connect("SG-spiky-nutria-5632-6037-mysql-master.servers.mongodirector.com", "admin", "MUCzzJ8hf!hfhGE", "securechatapp", 3306);
-    // $conn = new mysqli("SG-spiky-nutria-5632-6037-mysql-master.servers.mongodirector.com", "admin", "MUCzzJ8hf!hfhGE", "securechatapp", 3306);
+    // $conn = mysqli_connect("SG-spiky-nutria-5632-6037-mysql-master.servers.mongodirector.com", "admin", "MUCzzJ8hf!hfhGE", "securechatapp", 3306);
+    $conn = new mysqli("SG-spiky-nutria-5632-6037-mysql-master.servers.mongodirector.com", "sgroot", "Au28FOWP1.rcswOd", "securechatapp");
     if(!$conn){
         echo "Database not connected" . mysqli_connect_error();
     }
