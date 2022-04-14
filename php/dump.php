649fd25b394a9072334a1ee1530b20878a775d44
Cesar Aleman <cesar.aleman.cal@gmail.com>
2022-04-14 09:09:49 -0700

    simple attempt.

--- php/dump.php
+++ php/dump.php
@@ -1,15 +1,8 @@
 <?php
     session_start();
     if(isset($_SESSION['unique_id'])){
-        $dbhost = 'localhost:3036';
-        $dbuser = 'root';
-        $dbpass = '';
-        
-        $backup_file = $dbname . date("Y-m-d-H-i-s") . '.gz';
-        $command = "mysqldump --opt -h $dbhost -u $dbuser -p $dbpass ". "test_db | gzip > $backup_file";
-        
-        system($command);
-        // exec("mysqldump -u root -p securechatapp > securechatapp.sql");
+
+        exec("mysqldump -u root -p -h localhost securechatapp > securechatapp.sql");
         header("location: ../users.php");
     }
     else {
