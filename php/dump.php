21c3539f50163f4eccba33039b9f32d2bebe745a
Cesar Aleman <cesar.aleman.cal@gmail.com>
2022-04-14 08:58:03 -0700

    another dump

--- php/dump.php
+++ php/dump.php
@@ -1,9 +1,20 @@
 <?php
     session_start();
     if(isset($_SESSION['unique_id'])){
-        exec("mysqldump -u root -p securechatapp > securechatapp.sql");
+        $dir = "path/to/file/";
+        $filename = "backup" . date("YmdHis") . ".sql.gz";
+        $db_host = "localhost";
+        $db_username = "root";
+        $db_password = "";
+        $db_database = "securechatapp";
+
+        $cmd = "mysqldump -h {$db_host} -u {$db_username} --password={$db_password} {$db_database} | gzip > securechatapp.sql";
+        exec($cmd);
+        // exec("mysqldump -u root -p securechatapp > securechatapp.sql");
+        header("location: ../users.php");
     }
     else {
+
         echo "you are not logged in";
     }
 ?>
