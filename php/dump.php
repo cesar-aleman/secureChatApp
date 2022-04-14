456c8a514f444ad5eddd9d9498ff3d1abd6539a3
Cesar Aleman <cesar.aleman.cal@gmail.com>
2022-04-14 09:04:35 -0700

    new attempt

--- php/dump.php
+++ php/dump.php
@@ -1,20 +1,19 @@
 <?php
     session_start();
     if(isset($_SESSION['unique_id'])){
-        $dir = "path/to/file/";
-        $filename = "backup" . date("YmdHis") . ".sql.gz";
-        $db_host = "localhost";
-        $db_username = "root";
-        $db_password = "";
-        $db_database = "securechatapp";
-
-        $cmd = "mysqldump -h {$db_host} -u {$db_username} --password={$db_password} {$db_database} | gzip > securechatapp.sql";
-        exec($cmd);
+        $database = 'securechatapp';
+        $user = 'root';
+        $pass = '';
+        $host = 'localhost';
+        $dir = dirname(__FILE__) . '/dump.sql';
+        
+        echo "<h3>Backing up database to `<code>{$dir}</code>`</h3>";
+        
+        exec("mysqldump --user={$user} --password={$pass} --host={$host} {$database} --result-file={$dir} 2>&1", $output);
         // exec("mysqldump -u root -p securechatapp > securechatapp.sql");
         header("location: ../users.php");
     }
     else {
-
         echo "you are not logged in";
     }
 ?>
