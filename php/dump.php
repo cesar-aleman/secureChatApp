7e09c929c3da4431694b130ced8766122e9a6b33
Cesar Aleman <cesar.aleman.cal@gmail.com>
2022-04-14 09:48:08 -0700

    attempting dump online

--- php/dump.php
+++ php/dump.php
@@ -2,7 +2,16 @@
     session_start();
     if(isset($_SESSION['unique_id'])){
 
-        exec("mysqldump -u root -p -h localhost securechatapp > securechatapp.sql");
+        $dbuser = 'bb8bdcf73f9d70';
+        $password = '6eb93168';
+        $dbname = 'heroku_e159c57b3a4bcfd';
+        $host = 'us-cdbr-east-05.cleardb.net';
+
+        // $command = "mysqldump -u $dbuser -h localhost -p$password $dbname > dump.sql 2>&1";
+        // exec($command, $output);
+
+        exec("mysqldump -u $dbuser -h $host -p$password $dbname > my_database_dump.sql");
+
         header("location: ../users.php");
     }
     else {
