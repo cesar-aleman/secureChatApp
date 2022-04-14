1f6165ba641947f5c12a25499b0f8001f21ec987
Cesar Aleman <cesar.aleman.cal@gmail.com>
2022-04-14 10:57:10 -0700

    different dump now.

--- php/dump.php
+++ php/dump.php
@@ -5,14 +5,58 @@
         include_once "config.php";
 
 
-        $user = "bb8bdcf73f9d70";
+        $username = "bb8bdcf73f9d70";
         $password = "6eb93168";
-        $dbname = "heroku_e159c57b3a4bcfd";
+        $database = "heroku_e159c57b3a4bcfd";
         $host = "us-cdbr-east-05.cleardb.net";
 
-        $sql = mysqli_query($conn, "SELECT * FROM users");
+        // $sql = mysqli_query($conn, "SELECT * FROM users");
+        // $row = mysqli_fetch_assoc($sql);
+        // echo $row;
 
+?>
+        <body>
+        <?php 
+        // $username = "root"; 
+        // $password = ""; 
+        // $database = "securechatapp"; 
+        // $host = "localhost";
+        $mysqli = new mysqli($host, $username, $password, $database); 
+        $query = "SELECT * FROM messages INNER JOIN users ON unique_id";
+        
+        
+        echo '<table border="1" cellspacing="10" cellpadding="10"> 
+              <tr> 
+                  <td> <font face="Arial">Sender</font> </td> 
+                  <td> <font face="Arial">Message ID </font> </td> 
+                  <td> <font face="Arial">Message </font> </td> 
+                  <td> <font face="Arial">Recipient</font> </td> 
+              </tr>';
+        
+        if ($result = $mysqli->query($query)) {
+            while ($row = $result->fetch_assoc()) {
+                $sender_id = $row["outgoing_msg_id"];
+                $recipient_id = $row ["incoming_msg_id"];
+                $msg = $row["msg"];
+                $msg_id = $row["msg_id"];
+                $fname = $row["fname"];
+                $unique_id = $row["unique_id"];
+              
+                if($unique_id == $sender_id){
+                    echo '<tr> 
+                        <td>'.$sender_id.'</td>
+                        <td>'.$msg_id.'</td>  
+                        <td>'.$msg.'</td> 
+                        <td>'.$recipient_id.'</td>  
+                    </tr>';
+                }
+            }
+            $result->free();
+        } 
+        ?>
+        </body>        
 
+<?php
         // header("location: ../users.php");
     }
     else {
