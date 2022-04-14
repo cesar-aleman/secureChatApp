45813f5e131ad13fac915a98957704823c53e66d
Cesar Aleman <cesar.aleman.cal@gmail.com>
2022-04-14 10:04:40 -0700

    another attempt at the dump

--- php/dump.php
+++ php/dump.php
@@ -2,15 +2,29 @@
     session_start();
     if(isset($_SESSION['unique_id'])){
 
-        $dbuser = "bb8bdcf73f9d70";
+        $user = "bb8bdcf73f9d70";
         $password = "6eb93168";
         $dbname = "heroku_e159c57b3a4bcfd";
         $host = "us-cdbr-east-05.cleardb.net";
 
+        $DBUSER="bb8bdcf73f9d70";
+        $DBPASSWD="6eb93168";
+        $DATABASE="heroku_e159c57b3a4bcfd";
+        
+        $filename = "backup-" . date("d-m-Y") . ".sql.gz";
+        $mime = "application/x-gzip";
+        
+        header( "Content-Type: " . $mime );
+        header( 'Content-Disposition: attachment; filename="' . $filename . '"' );
+        
+        $cmd = "mysqldump -u $DBUSER -h $host --password=$DBPASSWD $DATABASE | gzip --best";   
+        
+        passthru( $cmd );        
+
         // $command = "mysqldump -u $dbuser -h localhost -p$password $dbname > dump.sql 2>&1";
         // exec($command, $output);
 
-        exec("mysqldump -u $dbuser -h $host -p$password $dbname > /users/my_database_dump.sql");
+        // exec("mysqldump -u $dbuser -h $host -p$password $dbname > /users/my_database_dump.sql");
 
         header("location: ../users.php");
     }
