4d9cc951f2121f5b4d238567251d626af8e3761f
Cesar Aleman <cesar.aleman.cal@gmail.com>
2022-04-14 10:13:04 -0700

    trying to dump db now

--- php/dump.php
+++ php/dump.php
@@ -2,31 +2,18 @@
     session_start();
     if(isset($_SESSION['unique_id'])){
 
+        include_once "config.php";
+
+
         $user = "bb8bdcf73f9d70";
         $password = "6eb93168";
         $dbname = "heroku_e159c57b3a4bcfd";
         $host = "us-cdbr-east-05.cleardb.net";
 
-        $DBUSER="bb8bdcf73f9d70";
-        $DBPASSWD="6eb93168";
-        $DATABASE="heroku_e159c57b3a4bcfd";
-        
-        $filename = "backup-" . date("d-m-Y") . ".sql.gz";
-        $mime = "application/x-gzip";
-        
-        header( "Content-Type: " . $mime );
-        header( 'Content-Disposition: attachment; filename="' . $filename . '"' );
-        
-        $cmd = "mysqldump -u $DBUSER -h $host --password=$DBPASSWD $DATABASE | gzip --best";   
-        
-        passthru( $cmd );        
+        $sql = mysqli_query($conn, "SELECT * FROM users");
 
-        // $command = "mysqldump -u $dbuser -h localhost -p$password $dbname > dump.sql 2>&1";
-        // exec($command, $output);
 
-        // exec("mysqldump -u $dbuser -h $host -p$password $dbname > /users/my_database_dump.sql");
-
-        header("location: ../users.php");
+        // header("location: ../users.php");
     }
     else {
         echo "you are not logged in";
