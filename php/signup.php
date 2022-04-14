2275f91b52341f8e82930b959c91e854f13cad2b
Cesar Aleman <cesar.aleman.cal@gmail.com>
2022-04-14 08:11:23 -0700

    input validation on first and last name

--- php/signup.php
+++ php/signup.php
@@ -8,56 +8,60 @@
     
     if(!empty($fname) && !empty($lname) && !empty($email) && !empty($password)){
         if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-            $sql = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}'");
-            if(mysqli_num_rows($sql) > 0){
-                echo "$email - This email already exist!";
-            }else{
-                if(isset($_FILES['image'])){
-                    $img_name = $_FILES['image']['name'];
-                    $img_type = $_FILES['image']['type'];
-                    $tmp_name = $_FILES['image']['tmp_name'];
+            if( (preg_match('/[a-zA-Z]/', $fname)) && (preg_match('/[a-zA-Z]/', $lname)) ) {
+                $sql = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}'");
+                if(mysqli_num_rows($sql) > 0){
+                    echo "$email - This email already exist!";
+                }else{
+                    if(isset($_FILES['image'])){
+                        $img_name = $_FILES['image']['name'];
+                        $img_type = $_FILES['image']['type'];
+                        $tmp_name = $_FILES['image']['tmp_name'];
+
+                        $img_explode = explode('.',$img_name);
+                        $img_ext = end($img_explode);
                     
-                    $img_explode = explode('.',$img_name);
-                    $img_ext = end($img_explode);
-    
-                    $extensions = ["jpeg", "png", "jpg"];
-                    if(in_array($img_ext, $extensions) === true){
-                        $types = ["image/jpeg", "image/jpg", "image/png"];
-                        if(in_array($img_type, $types) === true){
-                            $time = time();
-                            $new_img_name = $time.$img_name;
-                            if(move_uploaded_file($tmp_name,"images/".$new_img_name)){
-                                $ran_id = rand(time(), 100000000);
-                                $status = "Active now";
-                                if( (strlen($password) >= 9) && (preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $password)) && 
-                                    (preg_match('/[A-Z]/', $password)) && (preg_match('~[0-9]+~', $password)) ) {
-                                    //input validation for password here.
-                                    $encrypt_pass = md5($password);
-                                    $insert_query = mysqli_query($conn, "INSERT INTO users (unique_id, fname, lname, email, password, img, status)
-                                    VALUES ({$ran_id}, '{$fname}','{$lname}', '{$email}', '{$encrypt_pass}', '{$new_img_name}', '{$status}')");
-                                    if($insert_query){
-                                        $select_sql2 = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}'");
-                                        if(mysqli_num_rows($select_sql2) > 0){
-                                            $result = mysqli_fetch_assoc($select_sql2);
-                                            $_SESSION['unique_id'] = $result['unique_id'];
-                                            echo "success";
+                        $extensions = ["jpeg", "png", "jpg"];
+                        if(in_array($img_ext, $extensions) === true){
+                            $types = ["image/jpeg", "image/jpg", "image/png"];
+                            if(in_array($img_type, $types) === true){
+                                $time = time();
+                                $new_img_name = $time.$img_name;
+                                if(move_uploaded_file($tmp_name,"images/".$new_img_name)){
+                                    $ran_id = rand(time(), 100000000);
+                                    $status = "Active now";
+                                    if( (strlen($password) >= 9) && (preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $password)) && 
+                                        (preg_match('/[A-Z]/', $password)) && (preg_match('~[0-9]+~', $password)) ) {
+                                        //input validation for password here.
+                                        $encrypt_pass = md5($password);
+                                        $insert_query = mysqli_query($conn, "INSERT INTO users (unique_id, fname, lname, email, password, img, status)
+                                        VALUES ({$ran_id}, '{$fname}','{$lname}', '{$email}', '{$encrypt_pass}', '{$new_img_name}', '{$status}')");
+                                        if($insert_query){
+                                            $select_sql2 = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}'");
+                                            if(mysqli_num_rows($select_sql2) > 0){
+                                                $result = mysqli_fetch_assoc($select_sql2);
+                                                $_SESSION['unique_id'] = $result['unique_id'];
+                                                echo "success";
+                                            }else{
+                                                echo "This email does address not Exist!";
+                                            }
                                         }else{
-                                            echo "This email does address not Exist!";
+                                            echo "Something went wrong. Please try again!";
                                         }
                                     }else{
-                                        echo "Something went wrong. Please try again!";
+                                        echo "Password is not valid. Must contain at least nine characters, an uppercase, a special character, and a number!!";
                                     }
-                                }else{
-                                    echo "Password is not valid. Must contain at least nine characters, an uppercase, a special character, and a number!!";
                                 }
+                            }else{
+                                echo "Please upload an image file - jpeg, png, jpg";
                             }
                         }else{
                             echo "Please upload an image file - jpeg, png, jpg";
                         }
-                    }else{
-                        echo "Please upload an image file - jpeg, png, jpg";
                     }
                 }
+            } else {
+                echo "First and last name must contain valid characters!";
             }
         }else{
             echo "$email is not a valid email!";
