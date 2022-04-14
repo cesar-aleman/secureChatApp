d6469fc22a4e4ade7238ace95f798ebf81e8cca9
Cesar Aleman <cesar.aleman.cal@gmail.com>
2022-04-14 11:06:25 -0700

    cleaning up code and adding credits

--- php/dbdump.php
+++ php/dbdump.php
@@ -1,18 +1,13 @@
+//Resource used to create this page: https://www.siteground.com/
 <?php
     session_start();
     if(isset($_SESSION['unique_id'])){
 
         include_once "config.php";
-
-
         $username = "bb8bdcf73f9d70";
         $password = "6eb93168";
         $database = "heroku_e159c57b3a4bcfd";
         $host = "us-cdbr-east-05.cleardb.net";
-
-        // $sql = mysqli_query($conn, "SELECT * FROM users");
-        // $row = mysqli_fetch_assoc($sql);
-        // echo $row;
 
 ?>
         <body>
@@ -23,7 +18,6 @@
         // $host = "localhost";
         $mysqli = new mysqli($host, $username, $password, $database); 
         $query = "SELECT * FROM messages INNER JOIN users ON unique_id";
-        
         
         echo '<table border="1" cellspacing="10" cellpadding="10"> 
               <tr> 
@@ -39,7 +33,6 @@
                 $recipient_id = $row ["incoming_msg_id"];
                 $msg = $row["msg"];
                 $msg_id = $row["msg_id"];
-                $fname = $row["fname"];
                 $unique_id = $row["unique_id"];
               
                 if($unique_id == $sender_id){
@@ -54,10 +47,8 @@
             $result->free();
         } 
         ?>
-        </body>        
-
+        </body>
 <?php
-        // header("location: ../users.php");
     }
     else {
         echo "you are not logged in";
